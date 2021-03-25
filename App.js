b750b3100970beefa4895c6b665283fc7b88a01b
anshulchaudhary <54776396+anschy@users.noreply.github.com>
2021-03-25 23:13:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Header';
+import Body from './Components/Body';
+// import Delete from './Components/Delete';
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Body/>
+    </div>
+  );
+}
+
+export default App;
